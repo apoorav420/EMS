8dde85709cf003a34dfa184a938068696df948aa
apoorav <apoorav@DESKTOP-GJOJM1M>
2021-07-27 11:46:25 +0530

    first repository

---
+++ instagram/src/main/java/com/instagram/dao/InstagramDAOInterface.java
@@ -0,0 +1,15 @@
+package com.instagram.dao;
+
+import com.instagram.entity.InstagramUser;
+
+public interface InstagramDAOInterface {
+
+	int createProfileDAO(InstagramUser iu);
+
+	InstagramUser viewProfile(InstagramUser iu);
+
+	int editProfileDAO(InstagramUser iu1);
+
+	int deleteProfileDAO(InstagramUser iu);
+
+}
